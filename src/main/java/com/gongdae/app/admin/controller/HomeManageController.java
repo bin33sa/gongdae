9dd5d90989f887ad191a7424fc2449413cccd70b
stayt <staytraveler@gmail.com>
2026-03-03 17:31:17 +0900

    관리자 컨트롤러 변경

--- src/main/java/com/gongdae/app/admin/controller/HomeManageController.java
+++ src/main/java/com/gongdae/app/admin/controller/HomeManageController.java
@@ -5,9 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.gongdae.app.domain.dto.SessionInfo;
-import com.gongdae.app.security.LoginMemberUtil;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HomeManageController {
 	 
-<<<<<<< HEAD
+
 	@GetMapping("/admin")
 	public String handleHome(Model model) {
 		 model.addAttribute("pageTitle", "관리자 메인");
@@ -30,25 +27,6 @@ public class HomeManageController {
 	    model.addAttribute("pageTitle", "사이트 매출");
 	    model.addAttribute("contentPage", "/WEB-INF/views/admin/menu/list.jsp");
 	    return "admin/layout";
-=======
-	@GetMapping("/login")
-	public String handleLogin() {
-		SessionInfo info = LoginMemberUtil.getSessionInfo();
-	    
-	    if (info != null) {
-	        if (info.getUserLevel() >= 50) { 
-	            return "redirect:/admin";
-	        } else {
-	            return "redirect:/";
-	        }
-	    }
-		return "admin/login";
-	}
-	
-	
-	@GetMapping({"", "/"})
-	public String handleHome() {
-		return "admin/main/home";
->>>>>>> branch 'main' of https://github.com/bin33sa/gongdae.git
+
 	}
 }
