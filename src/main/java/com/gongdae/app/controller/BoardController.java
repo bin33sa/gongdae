67b0483860f31fdd4674b8fad9a6109948da012b
unknown <alstjd4824@gmail.com>
2026-03-19 17:36:47 +0900

    minseong0319

--- src/main/java/com/gongdae/app/controller/BoardController.java
+++ src/main/java/com/gongdae/app/controller/BoardController.java
@@ -1,15 +1,7 @@
 package com.gongdae.app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.gongdae.app.domain.dto.SessionInfo;
-import com.gongdae.app.security.LoginMemberUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
